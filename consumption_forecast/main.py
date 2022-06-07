dbf78377f6ad5d5ee080b353f22d8472eef9b1fb
gryshkomv <gryshkomv@dtek.com>
2022-06-07 11:16:08 +0300

    Feature_temp_added

--- consumption_forecast/main.py
+++ consumption_forecast/main.py
@@ -1,5 +1,11 @@
 from thunder import Thunder
+from decouple import config as envs
+from pathlib import Path
+
 
 if __name__ == '__main__':
     # comment for common
+    path_files = envs('STORAGE_PATH', cast=str)
+    Path(path_files).mkdir(parents=True, exist_ok=True)
+    print('Folders has been created')
     Thunder().run()
